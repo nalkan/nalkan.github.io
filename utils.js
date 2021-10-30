12d52d109f90244aa001d459ebf43554c88ab88f
nalkan <93370724+nalkan@users.noreply.github.com>
2021-10-30 07:18:06 +0300

    test

---
+++ utils.js
@@ -0,0 +1,12 @@
+var FORMIO_ENDPOINT = 'https://9a0a-176-234-90-157.ngrok.io';
+function loadForm(formId) {
+    Formio.createForm(document.getElementById('formio'), FORMIO_ENDPOINT + '/form/' + formId).then(function (form) {
+        form.on('componentError', function (err) {
+            /** if (err.component.key === 'myselect' && err.message === 'Unauthorized') {
+                const select = form.getComponent('myselect');
+                select.updateItems();
+            }**/
+            console.log(err);
+        });
+    });
+}
