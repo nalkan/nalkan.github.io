60479df7b9e680f29a9cddfc39e2ea28bb4b0678
nalkan <93370724+nalkan@users.noreply.github.com>
2021-10-30 07:25:16 +0300

    test

--- utils.js
+++ utils.js
@@ -1,16 +1,23 @@
 var FORMIO_ENDPOINT = 'https://9a0a-176-234-90-157.ngrok.io';
 function loadForm(formId) {
-    Formio.createForm(document.getElementById('formio'), FORMIO_ENDPOINT + '/form/' + formId).then(function (form) {
-        form.on('componentError', function (err) {
-            /** if (err.component.key === 'myselect' && err.message === 'Unauthorized') {
-                const select = form.getComponent('myselect');
-                select.updateItems();
-            }**/
-            console.log(err);
+    try {
+        Formio.createForm(document.getElementById('formio'), FORMIO_ENDPOINT + '/form/' + formId).then(function (form) {
+            form.on('componentError', function (err) {
+                /** if (err.component.key === 'myselect' && err.message === 'Unauthorized') {
+                    const select = form.getComponent('myselect');
+                    select.updateItems();
+                }**/
+                console.log(err);
+            });
+        }).catch(e => {
+            console.log('Global error');
+            console.log(e);
+            console.log(JSON.stringify(e));
         });
-    }).catch(e => {
-        console.log('Global error');
-        console.log(e);
-        console.log(JSON.stringify(e));
-    });
+    } catch (ex) {
+        console.log('try-catch');
+        console.log(ex);
+        console.log(JSON.stringify(ex));
+
+    }
 }
